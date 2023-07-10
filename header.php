55c6f926337591b18ea7ea2aaae8da53549e456e
ST_MALIHA <81468017+s-maliha@users.noreply.github.com>
2023-07-10 18:04:03 +0600

    new comit

--- header.php
+++ header.php
@@ -9,12 +9,23 @@
     
 </head>
 <body>
+<<<<<<< Updated upstream
     <h1>Header added 
 
     hello 
     </h1>
 
+<<<<<<< Updated upstream
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ratione nihil eos similique nulla quas laborum, tempore reprehenderit repellendus fugiat?</p>
     
+=======
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, officia?</p>
+=======
+    <h1>Header added</h1>
+
+     
+    </p>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 </body>
 </html>
