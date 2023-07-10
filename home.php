e7ffccdd2701cc17eb2b052c23d3edf6aec4fc04
Mahadi Hasan <mhasan201431@bscse.uiu.ac.bd>
2023-07-10 19:39:16 +0600

    A div is added to the homr page

--- home.php
+++ home.php
@@ -9,5 +9,16 @@
     This is home page
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, dicta doloremque natus quod voluptatibus perferendis cupiditate facere quae aliquid dolorum ad optio vel obcaecati repellat expedita hic amet. Doloribus quae iste atque ad mollitia adipisci harum, ab, vero officia similique maxime ipsa libero facilis beatae ratione nobis tenetur itaque excepturi consequuntur ea dolore pariatur blanditiis, voluptatum eligendi? Maiores at animi exercitationem ducimus, suscipit autem atque consectetur pariatur necessitatibus minima iste nam veritatis facilis architecto est praesentium accusantium nisi repellendus voluptate, laboriosam numquam! Pariatur, distinctio. Deleniti facilis dolorem laudantium minima dignissimos? Maxime, aut. Ab est dolorem voluptas fugiat ipsum animi ad.</p>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat est cum perspiciatis quasi? Placeat eaque, voluptate inventore magni reiciendis illo amet nulla pariatur distinctio commodi architecto ab sapiente optio maxime?
+</p>
+
+<div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt harum voluptates placeat illo illum accusantium ut facilis dignissimos est doloribus.</p>
+</div>
+
 </body>
+
+
 </html>
